97ef7142218cd83a58a3646242b337e1787cce96
Marcela Melo <marcela@studiomoob.com>
2016-04-19 09:18:17 -0300

    Criação do projeto

--- mynotes/app/src/main/java/atpspc/mynotes/common/SMResponseListener.java
+++
@@ -1,9 +0,0 @@
-package com.studiomoob.fokas.common.smnetworking;
-
-public interface SMResponseListener
-{
-    void success(Object obj, SMNetworkingResponse response);
-
-    void fail(SMNetworkingError error);
-
-}
---
+++ mynotes/app/src/main/java/com/marcelamelo/mynotes/AppConfig.java
@@ -0,0 +1,7 @@
+package com.marcelamelo.mynotes;
+
+/**
+ * Created by StudioMoob on 19/04/2016.
+ */
+public class AppConfig {
+}
